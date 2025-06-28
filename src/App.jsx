531b3890b21aa4c5b44e6fd45646ebc029350541
PC <hoangtuan0796991061@gmail.com>
2025-06-28 03:15:54 +0000

    hehe

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Gltf, ScrollControls, useScroll } from "@react-three/drei";
 import { getProject, val } from "@theatre/core";
+import theatreState from "./theatreState.json";
 import studio from "@theatre/studio";
 import {
   SheetProvider,
@@ -31,12 +32,12 @@ function Scene() {
   const scroll = useScroll();
 
   // our callback will run on every animation frame
-  useFrame(() => {
-    // the length of our sequence
-    const sequenceLength = val(sheet.sequence.pointer.length);
-    // update the "position" of the playhead in the sequence, as a fraction of its whole length
-    sheet.sequence.position = scroll.offset * sequenceLength;
-  });
+  // useFrame(() => {
+  //   // the length of our sequence
+  //   const sequenceLength = val(sheet.sequence.pointer.length);
+  //   // update the "position" of the playhead in the sequence, as a fraction of its whole length
+  //   sheet.sequence.position = scroll.offset * sequenceLength;
+  // });
 
   const bgColor = "#84a4f4";
 
@@ -46,8 +47,8 @@ function Scene() {
       <fog attach="fog" color={bgColor} near={-4} far={10} />
       <ambientLight intensity={0.5} />
       <directionalLight position={[-5, 5, -5]} intensity={1.5} />
-      <Gltf src="/housecombined.glb" castShadow receiveShadow onError={console.error} />
-      <PerspectiveCamera  
+      <Gltf src="/HouseCombined.glb" castShadow receiveShadow />
+      <PerspectiveCamera
         theatreKey="Camera"
         makeDefault
         position={[0, 0, 0]}
