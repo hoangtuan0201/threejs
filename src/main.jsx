b3e86c1296f34a4ed5727e811009ce80986ff2b4
AlivRick <thuan01206667600@gmail.com>
2025-07-22 22:09:49 +0700

    up

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,13 @@ import studio from "@theatre/studio";
 import extension from "@theatre/r3f/dist/extension";
 import "./index.css";
 
+const CURRENT_VERSION = '2.0.1';
+const storedVersion = localStorage.getItem('app_version');
 
+if (storedVersion !== CURRENT_VERSION) {
+  localStorage.setItem('app_version', CURRENT_VERSION);
+  window.location.reload();
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <AppRouter />
