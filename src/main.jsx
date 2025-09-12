a11867621c2fecb26d9d8100afbdaae4a969d9c8
PC <hoangtuan0796991061@gmail.com>
2025-09-12 23:14:50 +0700

    update sequence

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import studio from "@theatre/studio";
 import extension from "@theatre/r3f/dist/extension";
 import "./index.css";
 
-const CURRENT_VERSION = '3.3.0';
+const CURRENT_VERSION = '3.4.0';
 const storedVersion = localStorage.getItem('app_version');
 
 if (storedVersion !== CURRENT_VERSION) {
